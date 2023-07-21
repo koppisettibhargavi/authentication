bf7f736396dc5d41f141cc3cb8d74b1b564e16e7
root <Enter you email ID here>
2023-07-21 09:36:59 +0000

    nextzen

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/cart" component={Cart} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+)
+export default App
---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,13 @@
+import {Redirect, Route} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import Home from '../Home'
+
+const ProtectedRoute = () => {
+  const token = Cookies.get('jwt-token')
+  if (token === undefined) {
+    return <Redirect to="/login" />
+  }
+  return <Route exact path="/" component={Home} />
+}
+export default ProtectedRoute
